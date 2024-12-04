773f2ddbdf089a2a545620b790880a64e3467ffa
Zfjob <zfjobspm@gmail.com>
2024-12-04 04:09:42 -0500

    Update DogeBall.h
    12.4 4

--- DogeBall.h
+++ DogeBall.h
@@ -6,14 +6,17 @@
 // Description: Serves as the organizer Header File for the DodgleBall.cpp (Main file) Links All class header Files
 //////////////////////////////////////////////
 
+//Source Files:
 #pragma once
-//source files
+
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <vector>
 #include <fstream>
-
+#include <stdio.h>  // <- These 3 are for Randomly Seeding the Pin
+#include <stdlib.h> // <-|
+#include <time.h>   // <-|
 
 //Remove?
 //*****************************************************************************\\
