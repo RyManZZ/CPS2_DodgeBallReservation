8a5f79d12e10155c05f2cd84484015bc19c49866
Zfjob <zfjobspm@gmail.com>
2024-12-04 04:13:32 -0500

    Update sedan.h
    12 4 4

--- sedan.h
+++ sedan.h
@@ -3,26 +3,27 @@
 // Course: Computational Problem Solving II
 // Developers: Ryan Karges & Zachary Fitzgerald
 // File: sedan.h
-// Description: Serves as Header File for the sedan class (sedan.cpp):
+// Description: Serves as Header File for the Sedan class (sedan.cpp):
 //////////////////////////////////////////////
+
+//Header Files:
 #pragma once
 
 #include "DogeBall.h"
 #include "vehicle.h"
 
-
 using namespace NS_Vehicle;
 
-namespace NS_Sedan { //creates a namespace for tree
-	class Sedan : public Vehicle { //creates the tree class as a child of plant
+namespace NS_Sedan { //creates a namespace for Sedan
+	class Sedan : public Vehicle { //creates the Sedan class as a child of Vehicle
 
 	private:
 		vector<char> seat_S;
 
 	public:
-		Sedan();             //constructor
-		void displaySedan(); //prints the truck image
-		char displaySeatSedan(int); //may not be used
-		bool seatCheckSedan(int); //checks to see if the seat is valid
+		Sedan();             //constructor.
+		void displaySedan(); //prints the Sedan image.
+		char displaySeatSedan(int); //may not be used.
+		bool seatCheckSedan(int); //checks to see if the seat is valid.
 	};
 }
