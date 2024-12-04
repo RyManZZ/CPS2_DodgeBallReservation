f18d0e8d9a73f76df9356ed7cb6cac506894b3e5
Zfjob <zfjobspm@gmail.com>
2024-12-04 04:14:52 -0500

    Update vehicle.cpp
    12 4 4

--- vehicle.cpp
+++ vehicle.cpp
@@ -1,17 +1,25 @@
-//test
+//////////////////////////////////////////////
+// DodgeBall Team Reservation Project
+// Course: Computational Problem Solving II
+// Developers: Ryan Karges & Zachary Fitzgerald
+// File: vehicle.cpp
+// Description: Contains Vehicle Class. Allows access to Vehicle Driver, and Vehicle Description information:
+//////////////////////////////////////////////
+
+//Header Files & NameSpace Definitions:
 #include "DogeBall.h"
 #include "vehicle.h"
 
 using namespace NS_Vehicle;
 
-//default
+//Default:
 Vehicle::Vehicle() {
 	string driverFirstName = "NULL";
 	string driverLastName = "NULL";
 	string color = "NULL";
 } 
 
-//sets the drives name
+//Sets the Driver's Name:
 void Vehicle::SetDriverName(string driverFirstName, string driverLastName, string color) {
 	this->driverFirstName = driverFirstName;
 	this->driverLastName = driverLastName;
