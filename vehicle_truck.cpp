70a74ae532c1c03100b5ec631e450eca43ef0288
Zfjob <zfjobspm@gmail.com>
2024-12-04 04:17:06 -0500

    Update vehicle_truck.cpp
    12 4 4

--- vehicle_truck.cpp
+++ vehicle_truck.cpp
@@ -1,4 +1,12 @@
+//////////////////////////////////////////////
+// DodgeBall Team Reservation Project
+// Course: Computational Problem Solving II
+// Developers: Ryan Karges & Zachary Fitzgerald
+// File: vehicle_truck.cpp
+// Description: Contains the Truck Vehicle Class. Stores and processes information on Truck Vehicle Seating arrangements:
+//////////////////////////////////////////////
 
+//Header Files & NameSpace Definitions:
 #include "DogeBall.h"
 #include "vehicle.h"
 #include "truck.h"
@@ -6,11 +14,12 @@
 using namespace NS_Truck;
 using namespace NS_Vehicle;
 
+
 Truck::Truck() {            //pos   val
 	seat_T.push_back('5');  // 0     5
 }
 
-//prints the truck image
+//prints the Truck Image:
 void Truck::displayTruck() {
 
 	//    Truck
@@ -30,7 +39,7 @@ void Truck::displayTruck() {
 	cout << "|X   " << seat_T.at(0) << "|";
 }
 
-//reutens back the vector of seat
+//Returns back the vector of Seat:
 char Truck::displySeatTruck() {
 	return seat_T.at(0);
 } 
@@ -39,17 +48,23 @@ char Truck::displySeatTruck() {
 //returns true if the seat is open
 //else false
 bool Truck::seatCheckTruck() {
+
 	string userInput;
+
 	if (seat_T.at(0) == '5') {
+
 		cout << "The front seat is avaliable, would you like to reserve it Y/N\n";
 		cout << "choice: "; cin >> userInput;
+
 		//conformation check to make the reservation
 		if (userInput == "Y" || userInput == "y") {
+
 			//update the truck to show the spot has been claimed
 			cout << "congrats, reservation made, returing to main.";
 
-			seat_T.at(0)= 'X';
-			return true;
+			seat_T.at(0)= 'X'; // <- Indicates Visually that the Seat is taken.
+
+			return true; // <- Returns a value to confirm the reservation went through.
 		}
 		else { //if user cancels reservation
 			cout << "reservation cancled, returing to main";
