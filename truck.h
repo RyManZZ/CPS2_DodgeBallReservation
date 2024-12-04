44d59098ce1a9f4184b5c2a870d94e4ffcb405e9
Zfjob <zfjobspm@gmail.com>
2024-12-04 04:14:08 -0500

    Update truck.h
    12 4 4

--- truck.h
+++ truck.h
@@ -3,26 +3,27 @@
 // Course: Computational Problem Solving II
 // Developers: Ryan Karges & Zachary Fitzgerald
 // File: truck.h
-// Description: Serves as Header File for the truck class (truck.cpp):
+// Description: Serves as Header File for the Truck class (truck.cpp):
 //////////////////////////////////////////////
+
+//Header Files:
 #pragma once
 
 #include "DogeBall.h"
 #include "vehicle.h"
 
-
 using namespace NS_Vehicle;
 
-namespace NS_Truck { //creates a namespace for tree
-	class Truck : public Vehicle { //creates the tree class as a child of plant
+namespace NS_Truck { //creates a namespace for Truck
+	class Truck : public Vehicle { //creates the Truck class as a child of Vehicle
 
 	private:
 		vector<char> seat_T;
 
 	public:
-		Truck();             //constructor
-		void displayTruck(); //prints the truck image
-		char displySeatTruck(); //may not be used
-		bool seatCheckTruck(); //checks to see if the seat is valid
+		Truck();             //constructor.
+		void displayTruck(); //prints the truck image.
+		char displySeatTruck(); //may not be used.
+		bool seatCheckTruck(); //checks to see if the seat is valid.
 	};
 }
